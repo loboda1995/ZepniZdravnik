3635d4a60fd1cc59bb9db3503e612b7dc9ec38a0
Luka Loboda <lobodaluka@gmail.com>
2016-11-17 18:54:24 +0100

    Dodano ustvarjanje novih opomnikov, urejanje obstoječih in brisanje.

--- app/src/main/java/com/example/tpoteam/zepnizdravnik/MainActivity.java
+++ app/src/main/java/com/example/tpoteam/zepnizdravnik/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tpoteam.zepnizdravnik;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,9 +10,17 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String fileNameWithNotifications = "notificationsFile";
+    public static final String nameOfExtra1 = "Notifications";
+    public static final String nameOfExtra2 = "ID";
 
     private ListView list;
     private NotificationAdapter adapter;
@@ -31,18 +40,16 @@ public class MainActivity extends AppCompatActivity {
         list = (ListView) findViewById(R.id.list);
         list.setOnItemClickListener(movieDetailsListener);
 
-        medicineNotifications.add(new MedicineNotification(1, "Lekadol"));
-        medicineNotifications.add(new MedicineNotification(2, "Aspirin"));
-        medicineNotifications.add(new MedicineNotification(1, "Lekadol"));
-        medicineNotifications.add(new MedicineNotification(2, "Aspirin"));
-        medicineNotifications.add(new MedicineNotification(1, "Lekadol"));
-        medicineNotifications.add(new MedicineNotification(2, "Aspirin"));
-        medicineNotifications.add(new MedicineNotification(1, "Lekadol"));
-        medicineNotifications.add(new MedicineNotification(2, "Aspirin"));
-        medicineNotifications.add(new MedicineNotification(1, "Lekadol"));
-        medicineNotifications.add(new MedicineNotification(2, "Aspirin"));
-        medicineNotifications.add(null);
-
+        /*
+        medicineNotifications.add(new MedicineNotification(1, "Zdravilo 1"));
+        medicineNotifications.add(new MedicineNotification(2, "Zdravilo 2"));
+        medicineNotifications.add(new MedicineNotification(3, "Zdravilo 3"));
+        medicineNotifications.add(new MedicineNotification(4, "Zdravilo 4"));
+        medicineNotifications.add(new MedicineNotification(5, "Zdravilo 5"));
+        medicineNotifications.add(new MedicineNotification(6, "Zdravilo 6"));
+        medicineNotifications.add(new MedicineNotification(7, "Zdravilo 7"));
+        */
+        medicineNotifications = getMedicineNotifications();
         adapter = new NotificationAdapter(this, medicineNotifications);
         list.setAdapter(adapter);
     }
@@ -58,10 +65,38 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
             final Intent details = new Intent(MainActivity.this, NotificationOverview.class);
-            MedicineNotification izbrani = medicineNotifications.get(arg2);
-            details.putExtra("Notification", izbrani);
+            details.putExtra(nameOfExtra1, medicineNotifications);
+            details.putExtra(nameOfExtra2, arg2);
             startActivity(details);
         }
     };
+
+    protected void onResume(){
+        super.onResume();
+
+        medicineNotifications = getMedicineNotifications();
+        adapter = new NotificationAdapter(this, medicineNotifications);
+        list.setAdapter(adapter);
+    }
+
+    private ArrayList<MedicineNotification> getMedicineNotifications()
+    {
+        InputStream fis = null;
+        ObjectInputStream ois = null;
+        File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileNameWithNotifications);
+        ArrayList<MedicineNotification> noti = new ArrayList<>();
+        try {
+
+            fis = new FileInputStream(f);
+            ois = new ObjectInputStream(fis);
+            noti = (ArrayList<MedicineNotification>)ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        noti.add(null);
+        return noti;
+    }
 
 }
--- app/src/main/java/com/example/tpoteam/zepnizdravnik/NotificationOverview.java
+++ app/src/main/java/com/example/tpoteam/zepnizdravnik/NotificationOverview.java
@@ -2,18 +2,32 @@ package com.example.tpoteam.zepnizdravnik;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 /**
  * Created by Luka Loboda on 16-Nov-16.
  */
 
 public class NotificationOverview extends AppCompatActivity {
+
+    private ArrayList<MedicineNotification>  medicineNotifications =  new ArrayList<>();
+    private int IDselected;
+    private MedicineNotification selectedNotification;
+
+    private EditText medicineNameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +36,100 @@ public class NotificationOverview extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        MedicineNotification noti = (MedicineNotification) this.getIntent().getSerializableExtra("Notification");
+        // Pridobimo vnosna polja
+        medicineNameInput = (EditText) findViewById(R.id.medicineName);
 
-        // Ce je noti == null pomeni, da ustvarjamo nov opomnik, sicer spreminajmo ze obstojecega
-        if(noti != null){
-            TextView medicineNameDisplay = (TextView) findViewById(R.id.medicineName);
-            medicineNameDisplay.setText(noti.medicineName);
+        medicineNotifications = (ArrayList<MedicineNotification>) this.getIntent().getSerializableExtra(MainActivity.nameOfExtra1);
+        IDselected = this.getIntent().getIntExtra(MainActivity.nameOfExtra2, 0);
+
+        // Ce je IDselected kaze na zadnji element pomeni, da ustvarjamo nov opomnik, sicer
+        // spreminajmo ze obstojecega
+        if(IDselected < medicineNotifications.size()-1){
+            selectedNotification = medicineNotifications.get(IDselected);
+
+            medicineNameInput.setText(selectedNotification.medicineName);
             //TODO: pridobiti in prikazati se ostale atribute
+        }else{
+
+        }
+
+        // Odzivi na klike na gumbe
+        Button gumb = (Button) findViewById(R.id.deleteButton);
+        gumb.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                delete();
+            }
+        });
+        gumb = (Button) findViewById(R.id.saveButton);
+        gumb.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
+    }
+
+    // Izbrise izbrani opomnik
+    private void delete(){
+        if(selectedNotification != null){
+            //TODO: prikaz sporocila, "Ali ste prepricani?"
+            deleteThisNotification();
+            finish();
         }
     }
 
+    // Shrani novoustvarjen opomnik ali pa shrani spremenjen opomnik
+    private void save(){
+        if(allInputIsValid()){
+            saveThisNotification();
+            //TODO: prikaz sporocila, "Uspesno shranjeno"
+        }
+    }
+
+    // Preveri ali so vrednosti vseh vnosnih polj podane in regularne
+    private boolean allInputIsValid(){
+        //TODO: implementacija preverjanja
+        return true;
+    }
+
+    private void saveThisNotification() {
+        String newMedicineName = medicineNameInput.getText().toString();
+        medicineNotifications.remove(medicineNotifications.size()-1);
+
+        // Ce je izbrani Notification == null, pomeni da ustvarjamo novega, sicer spreminajmo ze
+        // obstojecega
+        if(selectedNotification != null){
+            selectedNotification.medicineName = newMedicineName;
+        }else{
+            selectedNotification = new MedicineNotification(10, newMedicineName);
+            medicineNotifications.add(selectedNotification);
+        }
+
+        // Shranimo trenutne opomnike
+        writeObjectToFile(medicineNotifications, MainActivity.fileNameWithNotifications);
+    }
+
+    private void deleteThisNotification() {
+        medicineNotifications.remove(medicineNotifications.size()-1);
+        medicineNotifications.remove(IDselected);
+
+        // Shranimo trenutne opomnike
+        writeObjectToFile(medicineNotifications, MainActivity.fileNameWithNotifications);
+    }
+
+    private void writeObjectToFile(Object o, String fileName){
+        try {
+            File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
+            FileOutputStream fileOutputStream = new FileOutputStream(f);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(o);
+            objectOutputStream.close();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
